47b1c9402a5728a0e355b369383afb68ade224d0
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-01-01 18:46:21 +0600

    Setting up necessary permission

--- app/src/main/java/com/bitmavrick/foreground_timer/MainActivity.kt
+++ app/src/main/java/com/bitmavrick/foreground_timer/MainActivity.kt
@@ -22,25 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    // Code from here
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ForegroundTimerTheme {
-        Greeting("Android")
     }
 }
