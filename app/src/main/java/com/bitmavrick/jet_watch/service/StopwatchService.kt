398c8f057ab383b71ec179eb3f773a6f0fc11ae3
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-01-02 18:17:32 +0600

    Done with the service helper

--- app/src/main/java/com/bitmavrick/jet_watch/service/StopwatchService.kt
+++ app/src/main/java/com/bitmavrick/jet_watch/service/StopwatchService.kt
@@ -1,6 +1,13 @@
 package com.bitmavrick.jet_watch.service
 
-class StopwatchService {
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class StopwatchService : Service(){
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
 
 }
 
