de7a52659f93887ee4dbfa50002b9f1913e6b9e8
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-01-03 19:49:31 +0600

    Done with the service

--- app/src/main/java/com/bitmavrick/jet_watch/service/StopwatchService.kt
+++ app/src/main/java/com/bitmavrick/jet_watch/service/StopwatchService.kt
@@ -1,13 +1,19 @@
 package com.bitmavrick.jet_watch.service
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.core.app.NotificationCompat
+import com.bitmavrick.jet_watch.util.Constants.NOTIFICATION_CHANNEL_ID
+import com.bitmavrick.jet_watch.util.Constants.NOTIFICATION_CHANNEL_NAME
+import com.bitmavrick.jet_watch.util.Constants.NOTIFICATION_ID
+import com.bitmavrick.jet_watch.util.formatTime
 import com.bitmavrick.jet_watch.util.pad
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Timer
@@ -57,6 +63,19 @@ class StopwatchService : Service(){
         }
     }
 
+    private fun stopStopwatch(){
+        if(this::timer.isInitialized){
+            timer.cancel()
+        }
+        currentState.value = StopwatchState.Stopped
+    }
+
+    private fun cancelStopwatch(){
+        duration = Duration.ZERO
+        currentState.value = StopwatchState.Idle
+        updateTimeUnits()
+    }
+
     private fun updateTimeUnits() {
         duration.toComponents { hours, minutes, seconds, _ ->
             this@StopwatchService.hours.value = hours.toInt().pad()
@@ -64,6 +83,72 @@ class StopwatchService : Service(){
             this@StopwatchService.seconds.value = seconds.pad()
         }
     }
+
+
+    private fun startForegroundService(){
+        createNotificationChannel()
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    private fun stopForegroundService() {
+        notificationManager.cancel(NOTIFICATION_ID)
+        stopForeground(STOP_FOREGROUND_REMOVE)
+        stopSelf()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                NOTIFICATION_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_LOW
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun updateNotification(hours: String, minutes: String, seconds: String) {
+        notificationManager.notify(
+            NOTIFICATION_ID,
+            notificationBuilder.setContentText(
+                formatTime(
+                    hours = hours,
+                    minutes = minutes,
+                    seconds = seconds,
+                )
+            ).build()
+        )
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun setStopButton() {
+        notificationBuilder.mActions.removeAt(0)
+        notificationBuilder.mActions.add(
+            0,
+            NotificationCompat.Action(
+                0,
+                "Stop",
+                ServiceHelper.stopPendingIntent(this)
+            )
+        )
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun setResumeButton() {
+        notificationBuilder.mActions.removeAt(0)
+        notificationBuilder.mActions.add(
+            0,
+            NotificationCompat.Action(
+                0,
+                "Resume",
+                ServiceHelper.resumePendingIntent(this)
+            )
+        )
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+
 
     inner class StopwatchBinder : Binder(){
         fun getService() : StopwatchService = this@StopwatchService
