808172dffcb7b9133d865999c4383f3a79777669
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-01-03 20:18:17 +0600

    Done with the dependency injection

--- app/src/main/java/com/bitmavrick/jet_watch/di/AppModule.kt
+++ app/src/main/java/com/bitmavrick/jet_watch/di/AppModule.kt
@@ -1,4 +1,42 @@
 package com.bitmavrick.jet_watch.di
 
-class AppModule {
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import com.bitmavrick.jet_watch.R
+import com.bitmavrick.jet_watch.service.ServiceHelper
+import com.bitmavrick.jet_watch.util.Constants.NOTIFICATION_CHANNEL_ID
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+
+@Module
+@InstallIn(ServiceComponent::class)
+object AppModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideNotificationBuilder(
+        @ApplicationContext context: Context
+    ): NotificationCompat.Builder {
+        return NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle("Jet-Watch")
+            .setContentText("00:00:00")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setOngoing(true)
+            .addAction(0, "Stop", ServiceHelper.stopPendingIntent(context))
+            .addAction(0, "Reset", ServiceHelper.cancelPendingIntent(context))
+            .setContentIntent(ServiceHelper.clickPendingIntent(context))
+    }
+
+    @ServiceScoped
+    @Provides
+    fun provideNotificationManager(
+        @ApplicationContext context: Context
+    ): NotificationManager {
+        return context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    }
 }
