34d5a8f6f562511e8b7fb39d4fdd8508026eee3f
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-01-04 22:28:12 +0600

    Done for today

--- app/src/main/java/com/bitmavrick/jet_watch/MainScreen.kt
+++ app/src/main/java/com/bitmavrick/jet_watch/MainScreen.kt
@@ -1,18 +1,24 @@
 package com.bitmavrick.jet_watch
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.bitmavrick.jet_watch.service.StopwatchService
 
@@ -39,6 +45,23 @@ fun MainScreen(stopwatchService: StopwatchService) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Text(
+                text = hours
+            )
+
+            Text(
+                text = minutes
+            )
+
+            Text(
+                text = seconds
+            )
+        }
+
+        Row(modifier = Modifier.weight(weight = 1f)) {
+            TextButton(onClick = { /*TODO*/ }) {
+
+            }
 
         }
     }
