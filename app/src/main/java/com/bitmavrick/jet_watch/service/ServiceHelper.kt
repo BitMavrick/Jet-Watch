4cb1652dbe2eaa1688d4374da7d2c9edda961312
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-01-06 13:43:24 +0600

    [android:enabled="true"] inside service, I make it false, that was the error. Solved!

--- app/src/main/java/com/bitmavrick/jet_watch/service/ServiceHelper.kt
+++ app/src/main/java/com/bitmavrick/jet_watch/service/ServiceHelper.kt
@@ -16,41 +16,37 @@ object ServiceHelper {
 
     private const val flag = PendingIntent.FLAG_IMMUTABLE
 
-    fun clickPendingIntent(context: Context) : PendingIntent {
+    fun clickPendingIntent(context: Context): PendingIntent {
         val clickIntent = Intent(context, MainActivity::class.java).apply {
             putExtra(STOPWATCH_STATE, StopwatchState.Started.name)
         }
-
         return PendingIntent.getActivity(
             context, CLICK_REQUEST_CODE, clickIntent, flag
         )
     }
 
-    fun stopPendingIntent(context: Context) : PendingIntent{
+    fun stopPendingIntent(context: Context): PendingIntent {
         val stopIntent = Intent(context, StopwatchService::class.java).apply {
             putExtra(STOPWATCH_STATE, StopwatchState.Stopped.name)
         }
-
         return PendingIntent.getService(
             context, STOP_REQUEST_CODE, stopIntent, flag
         )
     }
 
-    fun resumePendingIntent(context: Context) : PendingIntent {
+    fun resumePendingIntent(context: Context): PendingIntent {
         val resumeIntent = Intent(context, StopwatchService::class.java).apply {
             putExtra(STOPWATCH_STATE, StopwatchState.Started.name)
         }
-
         return PendingIntent.getService(
             context, RESUME_REQUEST_CODE, resumeIntent, flag
         )
     }
 
-    fun cancelPendingIntent(context: Context) : PendingIntent {
+    fun cancelPendingIntent(context: Context): PendingIntent {
         val cancelIntent = Intent(context, StopwatchService::class.java).apply {
             putExtra(STOPWATCH_STATE, StopwatchState.Canceled.name)
         }
-
         return PendingIntent.getService(
             context, CANCEL_REQUEST_CODE, cancelIntent, flag
         )
