245602d46e4cfc18b09b71b12bc688f34e49519c
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-10-16 16:36:30 +0600

    Saving the state

--- app/src/main/java/com/bitmavrick/jet_watch/activity/MainActivity.kt
+++ app/src/main/java/com/bitmavrick/jet_watch/activity/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.bitmavrick.jet_watch.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.bitmavrick.jet_watch.ui.home.HomeScreen
+import com.bitmavrick.jet_watch.ui.permission.PermissionScreen
 import com.bitmavrick.jet_watch.ui.theme.JetWatchTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge(
@@ -25,7 +32,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    if(checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED){
+                        HomeScreen()
+                    }else{
+                        PermissionScreen()
+                    }
                 }
             }
         }
--- app/src/main/java/com/bitmavrick/jet_watch/ui/permission/PermissionScreen.kt
+++ app/src/main/java/com/bitmavrick/jet_watch/ui/permission/PermissionScreen.kt
@@ -1,4 +1,94 @@
 package com.bitmavrick.jet_watch.ui.permission
 
-class PermissionScreen {
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionStatus
+import com.google.accompanist.permissions.rememberPermissionState
+
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun PermissionScreen(){
+    val context = LocalContext.current
+    val notificationPermissionState = rememberPermissionState(permission = Manifest.permission.POST_NOTIFICATIONS)
+
+    // * Tracks the permission
+    var hasRequestedPermission by rememberSaveable { mutableStateOf(false) }
+    var permissionRequestCompleted by rememberSaveable { mutableStateOf(false) }
+
+    LaunchedEffect( notificationPermissionState.status) {
+        if( hasRequestedPermission){
+            permissionRequestCompleted = true
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        when(val status = notificationPermissionState.status){
+            is PermissionStatus.Granted -> {
+                Text("Permission is Granted!")
+            }
+
+            is PermissionStatus.Denied -> {
+                if (permissionRequestCompleted){
+                    if (status.shouldShowRationale){
+                        Text("Notification permission is required to use this feature")
+                        Button(
+                            onClick = {
+                                notificationPermissionState.launchPermissionRequest()
+                                hasRequestedPermission = true
+                            }
+
+                        ) {
+                            Text("Request Notification Permission")
+                        }
+                    }else{
+                        Text("Notification Permission is denied completely. Please enable it from the settings menu")
+                        Button(
+                            onClick = {
+                                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                                    data = Uri.fromParts("package", context.packageName, null)
+                                }
+                                context.startActivity(intent)
+                            }
+                        ){
+                            Text("Open App Settings")
+                        }
+                    }
+                }else{
+                    Button(onClick = {
+                        notificationPermissionState.launchPermissionRequest()
+                        hasRequestedPermission = true
+                    }) {
+                        Text("Request Notification Permission")
+                    }
+                }
+            }
+        }
+    }
 }
